4ece4865d9162657ea143afc555281e5ce37f159
valenayelen <valenayelen651@gmail.com>
2026-09-15 20:42:29 -0300

    Agrego clase Entrega

---
+++ entrega.py
@@ -0,0 +1,8 @@
+class Entrega:
+
+    def __init__(self, id_entrega, fecha_entrega, hora_entrega, estado, ubicacion_actual):
+        self.id_entrega = id_entrega
+        self.fecha_entrega = fecha_entrega
+        self.hora_entrega = hora_entrega
+        self.estado = estado
+        self.ubicacion_actual = ubicacion_actual
