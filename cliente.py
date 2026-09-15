f72a54773c33e4132df20c169a05bfb60f3ce363
valenayelen <valenayelen651@gmail.com>
2026-09-15 20:37:06 -0300

    Agrego clase Cliente

---
+++ cliente.py
@@ -0,0 +1,9 @@
+class Cliente:
+
+    def __init__(self, id_cliente, nombre, apellido, telefono, email, direccion):
+        self.id_cliente = id_cliente
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.email = email
+        self.direccion = direccion
