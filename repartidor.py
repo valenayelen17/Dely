22ee799af0f00680d287c0a11740e7eeef0cd79e
valenayelen <valenayelen651@gmail.com>
2026-09-15 20:40:46 -0300

    Agrego clase Repartidor

---
+++ repartidor.py
@@ -0,0 +1,8 @@
+class Repartidor:
+
+    def __init__(self, id_repartidor, nombre, apellido, telefono, zona):
+        self.id_repartidor = id_repartidor
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.zona = zona
