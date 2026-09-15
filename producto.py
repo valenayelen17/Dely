36781c250e41d29356ca3164697f99ca81c5b12a
valenayelen <valenayelen651@gmail.com>
2026-09-15 20:43:18 -0300

    Agrego clase Producto

---
+++ producto.py
@@ -0,0 +1,9 @@
+class Producto:
+
+    def __init__(self, id_producto, nombre, descripcion, precio, categoria, stock):
+        self.id_producto = id_producto
+        self.nombre = nombre
+        self.descripcion = descripcion
+        self.precio = precio
+        self.categoria = categoria
+        self.stock = stock
